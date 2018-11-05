bc90e05cf7e1d1dc6f61bab6c2157cbd70eabfdf
WIL PANNELL <wgpannel@us.ibm.com>
2018-11-05 09:34:40 -0800

    0001-first test—-01-design with compiler errors

---
+++ src/test/java/com/ibm/cloud/garage/kata/bottles/tests/NinetyNineBottlesShould.java
@@ -0,0 +1,18 @@
+package com.ibm.cloud.garage.kata.bottles.tests;
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.*;
+
+public class NinetyNineBottlesShould {
+  @Test
+  public void performVerse99() throws Exception {
+    String expected =
+        "99 bottles of beer on the wall, " +
+        "99 bottles of beer.\n" +
+        "Take one down and pass it around, " +
+        "98 bottles of beer on the wall.\n";
+
+    Bottles bottles = new Bottles();
+    assertThat(bottles.verse(99)).isEqualTo(expected);
+  }
+}
