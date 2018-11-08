3e4c150fba583075444d44077c14d519138c56a8
WIL PANNELL <wgpannel@us.ibm.com>
2018-11-07 19:49:00 -0800

    0007-verses(n, m)—-01-verify boundary with obvious test; make test bar green

--- src/test/java/com/ibm/cloud/garage/kata/bottles/tests/NinetyNineBottlesShouldPlay.java
+++ src/test/java/com/ibm/cloud/garage/kata/bottles/tests/NinetyNineBottlesShouldPlay.java
@@ -88,6 +88,27 @@ public class NinetyNineBottlesShouldPlay {
     assertThat(bottles.verses(99, 98)).isEqualTo(expected);
   }
 
+  @Test
+  public void verses02to00() {
+    String expected =
+        "2 bottles of beer on the wall, " +
+        "2 bottles of beer.\n" +
+        "Take one down and pass it around, " +
+        "1 bottle of beer on the wall.\n" +
+        "\n" +
+        "1 bottle of beer on the wall, " +
+        "1 bottle of beer.\n" +
+        "Take it down and pass it around, " +
+        "no more bottles of beer on the wall.\n" +
+        "\n" +
+        "No more bottles of beer on the wall, " +
+        "no more bottles of beer.\n" +
+        "Go to the store and buy some more, " +
+        "99 bottles of beer on the wall.\n";
+
+    assertThat(bottles.verses(2, 0)).isEqualTo(expected);
+  }
+
   private class Bottles {
     String verse(int numberOfBottles) {
       if(numberOfBottles == 0)
