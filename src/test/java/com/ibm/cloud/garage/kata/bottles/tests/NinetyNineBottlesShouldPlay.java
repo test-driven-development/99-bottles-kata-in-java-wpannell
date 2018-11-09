8c6963b58276697077ab20312ee975f35bebb2f4
WIL PANNELL <wgpannel@us.ibm.com>
2018-11-09 15:53:25 -0800

    0018-predecessor—-04-introduce parameter

--- src/test/java/com/ibm/cloud/garage/kata/bottles/tests/NinetyNineBottlesShouldPlay.java
+++ src/test/java/com/ibm/cloud/garage/kata/bottles/tests/NinetyNineBottlesShouldPlay.java
@@ -116,7 +116,7 @@ public class NinetyNineBottlesShouldPlay {
             capitalize(quantity(numberOfBottles)) + " " + container(numberOfBottles) + " of beer on the wall, " +
             quantity(numberOfBottles) + " " + container(numberOfBottles) + " of beer.\n" +
             action(numberOfBottles) + ", " +
-            quantity(predecessor()) + " " + container(99) + " of beer on the wall.\n";
+            quantity(predecessor(numberOfBottles)) + " " + container(99) + " of beer on the wall.\n";
 
       return
           capitalize(quantity(numberOfBottles)) + " " + container(numberOfBottles) + " of beer on the wall, " +
