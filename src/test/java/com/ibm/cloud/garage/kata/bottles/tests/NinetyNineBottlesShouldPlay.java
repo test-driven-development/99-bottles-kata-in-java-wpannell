723277ed8d313a5c5d926d3f56997adde57ed2f4
WIL PANNELL <wgpannel@us.ibm.com>
2018-11-09 23:11:01 -0800

    0019-primitive-obsession—-02-move class

--- src/test/java/com/ibm/cloud/garage/kata/bottles/tests/NinetyNineBottlesShouldPlay.java
+++ src/test/java/com/ibm/cloud/garage/kata/bottles/tests/NinetyNineBottlesShouldPlay.java
@@ -1,11 +1,8 @@
 package com.ibm.cloud.garage.kata.bottles.tests;
 
+import com.ibm.cloud.garage.kata.bottles.Bottles;
 import org.junit.Before;
 import org.junit.Test;
-
-import java.util.function.IntUnaryOperator;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -109,54 +106,4 @@ public class NinetyNineBottlesShouldPlay {
     assertThat(bottles.verses(2, 0)).isEqualTo(expected);
   }
 
-  private class Bottles {
-    public String verse(int numberOfBottles) {
-      return
-          capitalize(quantity(numberOfBottles)) + " " + container(numberOfBottles) + " of beer on the wall, " +
-          quantity(numberOfBottles) + " " + container(numberOfBottles) + " of beer.\n" +
-          action(numberOfBottles) + ", " +
-          quantity(predecessor(numberOfBottles)) + " " + container(predecessor(numberOfBottles)) + " of beer on the wall.\n";
-    }
-
-    public String verses(int start, int end) {
-      return IntStream.rangeClosed(end, start)
-      .map(countDownFromTo(start, end))
-      .mapToObj(this::verse)
-      .collect(Collectors.joining("\n"));
-    }
-
-    public String song() { return verses(99, 0); }
-
-    private String action(int numberOfBottles) {
-      if(numberOfBottles == 0) return "Go to the store and buy some more";
-      return "Take " + pronoun(numberOfBottles) + " down and pass it around";
-    }
-
-    private String capitalize(String word) {
-      return word.toUpperCase().charAt(0) + word.substring(1);
-    }
-
-    private String container(int numberOfBottles) {
-      if(numberOfBottles == 1) return "bottle";
-      return "bottles";
-    }
-
-    private IntUnaryOperator countDownFromTo(int from, int to) {
-      return j -> to - j + from;
-    }
-
-    private int predecessor(int numberOfBottles) {
-      return ((numberOfBottles % -100) + 99) % 100;
-    }
-
-    private String pronoun(int numberOfBottles) {
-      if(numberOfBottles == 1) return "it";
-      return "one";
-    }
-
-    private String quantity(int numberOfBottles) {
-      if(numberOfBottles == 0) return "no more";
-      return String.valueOf(numberOfBottles);
-    }
-  }
 }
